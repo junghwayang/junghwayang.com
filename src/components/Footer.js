358c52539d7aeb06d71b84a61787ceda7aa19103
Junghwa Yang <rosie.junghwa@gmail.com>
2020-07-29 14:48:06 +1000

    ✏️ Fix typos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ const Footer = props => {
   let divider;
 
   if (props.page === 'index') {
-    divider = <svg className='footer-wave-divider' xmlns="http://www.w3.org/2000/svg" viewBox="0 30 1440 150"><path fill="#fff" fill-opacity="1" d="M0,96L80,85.3C160,75,320,53,480,64C640,75,800,117,960,133.3C1120,149,1280,139,1360,133.3L1440,128L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+    divider = <svg className='footer-wave-divider' xmlns="http://www.w3.org/2000/svg" viewBox="0 30 1440 150"><path fill="#fff" fillOpacity="1" d="M0,96L80,85.3C160,75,320,53,480,64C640,75,800,117,960,133.3C1120,149,1280,139,1360,133.3L1440,128L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
   } else {
     divider = <div className='footer-scallop-divider'></div>
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ const Header = props => {
           </div>
         </div>
 
-        <svg className='header-wave-divider' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 150'><path fill='white' fill-opacity='1' d='M0,96L80,85.3C160,75,320,53,480,64C640,75,800,117,960,133.3C1120,149,1280,139,1360,133.3L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z'></path></svg>
+        <svg className='header-wave-divider' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 150'><path fill='white' fillOpacity='1' d='M0,96L80,85.3C160,75,320,53,480,64C640,75,800,117,960,133.3C1120,149,1280,139,1360,133.3L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z'></path></svg>
       </header>
     );
   } else {
