44a7c737c86142dbcc705ebdd60c1caf95130b0c
Junghwa Yang <rosie.junghwa@gmail.com>
2020-07-29 15:01:54 +1000

    🐛 Fix bug to send contact form to Netlify

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,6 +3,7 @@ import React from 'react';
 const ContactForm = () => {
   return (
     <form className='contact-form' name='contact' method='POST' action='/success' data-netlify='true'>
+      <input type='hidden' name='form-name' value='contact' />
       <div className='form-input'>
         <input type='text' name='name' placeholder='Name' required />
         <input type='email' name='email' placeholder='Email' required />
