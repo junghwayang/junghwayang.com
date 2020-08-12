b9df02f2ce26fc259ca5e3a20c26b1e6d6c3c295
Junghwa Yang <rosie.junghwa@gmail.com>
2020-08-12 12:01:24 +1000

    💄 Add skills and icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => {
     this.img = `./icons/${name.toLowerCase().replace(/[\.\/]/g, '')}.${name === 'Apollo' ? 'svg' : 'png'}`;
   }
 
-  const skills = ['Node.js', 'Express', 'JavaScript', 'MongoDB', 'AWS', 'API', 'React', 'GraphQL', 'Gatsby', 'Prisma', 'Apollo', 'HTML', 'CSS', 'Sass/SCSS', 'Git', 'GitHub', 'CI/CD', 'Jenkins', 'Redis', 'MySQL', 'Testing'];
+  const skills = ['Node.js', 'Express', 'JavaScript', 'MongoDB', 'AWS', 'API', 'React', 'GraphQL', 'Gatsby', 'Prisma', 'Apollo', 'EJS', 'Pug', 'Jest', 'Git', 'GitHub', 'MySQL', 'CI/CD', 'Jenkins', 'Redis', 'HTML5', 'CSS3', 'Sass/SCSS', 'TDD'];
 
   return (
     <Layout
