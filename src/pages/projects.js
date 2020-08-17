b7b1119a78330ea692ff80a8f5ddfb40478241a8
Junghwa Yang <rosie.junghwa@gmail.com>
2020-08-17 22:50:20 +1000

    📝 Edit projects page content

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -20,8 +20,8 @@ const ProjectsPage = () => {
         />
         <ProjectCard
           title='BEPL'
-          skills={['Node.js', 'Express', 'MongoDB', 'Passport', 'EJS', 'JavaScript', 'HTML/CSS']}
-          description='Server-side rendered website for sharing beautiful places from all over the world.'
+          skills={['Node.js', 'Express', 'MongoDB', 'Passport', 'EJS']}
+          description='Server-side rendered website for sharing beautiful places from all over the world. Supports user authentication, authorization, photo uploads, and reviews.'
           img='./project-images/bepl.gif'
           demo='https://be-pl.herokuapp.com'
           code='https://github.com/junghwayang/bepl'
@@ -29,7 +29,7 @@ const ProjectsPage = () => {
         <ProjectCard
           title='Portfolio Website'
           skills={['Gatsby', 'React', 'GraphQL', 'JavaScript', 'Markdown', 'SCSS']}
-          description='Responsive personal portfolio & blog website'
+          description='Responsive personal portfolio & blog website. No theme was used and all components are designed by me.'
           img='./project-images/website.gif'
           demo='https://junghwayang.com'
           code='https://github.com/junghwayang/junghwayang.com'
@@ -37,31 +37,31 @@ const ProjectsPage = () => {
         <ProjectCard
           title='Byte.ly'
           skills={['Node.js', 'Express', 'MongoDB', 'JavaScript', 'HTML', 'CSS']}
-          description='A simple URL shortener. Hosted on Heroku.'
+          description='Simple URL shortener supporting API and front-end view. Supports to copy the shortened link to the clipboard. Hosted on Heroku.'
           img='./project-images/bytely.gif'
           demo='https://bytely-jh.herokuapp.com'
           code='https://github.com/junghwayang/byte.ly'
         />
         <ProjectCard
           title='Task-manager API'
-          skills={['Node.js', 'Express', 'MongoDB', 'JavaScript']}
-          description='Simple task-manager API'
+          skills={['Node.js', 'Express', 'MongoDB', 'Jest']}
+          description='RESTful API for task manager. Supports CRUD for task and user, and user authentication. Testing with Jest.'
           img='./project-images/task.jpg'
           demo='https://task-api-jh.herokuapp.com'
           code='https://github.com/junghwayang/task-api'
         />
         <ProjectCard
           title='Chat App'
-          skills={['Socket.io', 'Node.js', 'Express']}
-          description='Realtime web chat application with Socket.io, Node.js, Express'
+          skills={['Socket.io', 'Node.js', 'Express', 'JavaScript', 'HTML', 'CSS']}
+          description='Realtime web chat application with Socket.io. Supports to join the chat by name of the room, to filter bad words, and to share the current location of the user via Google maps.'
           img='./project-images/chatapp.gif'
           demo='https://chatapp-jh.herokuapp.com'
           code='https://github.com/junghwayang/chat-app'
         />
         <ProjectCard
           title='Weather App'
-          skills={['Node.js', 'Express']}
-          description='Simple weather app using Node.js, Express, and Dark sky & MapBox APIs'
+          skills={['Node.js', 'Express', 'EJS']}
+          description='Simple weather app using Dark sky & MapBox APIs. Use MapBox API to get the geocode for the city user enters, and Dark sky API to get weather by a geocode. Server-side rendered by EJS.'
           img='./project-images/weatherapp.gif'
           demo='https://weather-jh.herokuapp.com'
           code='https://github.com/junghwayang/weather-app'
